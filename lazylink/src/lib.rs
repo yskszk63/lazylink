883c301e28f2e17b606bc29a43b5a51e64245b30
yskszk63 <yskszk63@gmail.com>
2020-10-18 12:51:34 +0900

    bump version

--- lazylink/src/lib.rs
+++ lazylink/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/lazylink/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/lazylink/0.1.2")]
 //! Convert extern fn to libdl call procedural macro.
 //!
 //! # Example
